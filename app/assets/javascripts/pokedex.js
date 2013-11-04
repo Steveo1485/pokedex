c1577dbbd817b9a02da33bec3edb87f4fce1b76a
bbeaird <apprentice@dbc18.local>
2013-11-03 16:34:50 -0800

    Random Pokemon AJAXified the Rails way

--- app/assets/javascripts/pokedex.js
+++ app/assets/javascripts/pokedex.js
@@ -1,29 +1,41 @@
-var RandomizeHandler = {
+// The jQuery Way
+// var RandomizeHandler = {
 
-  init: function(){
-    $('form.random_pokemon').click(RandomizeHandler.randomPokemon)
-  },
+//   init: function(){
+//     $('form.random_pokemon').click(RandomizeHandler.randomPokemon)
+//   },
 
-  randomPokemon: function(e){
-    e.preventDefault();
-    var $newPokemonButton = $(this);
-    $.ajax({
-      url: $newPokemonButton.prop('action'),
-      type: $newPokemonButton.prop('method')
-    }).done(function(newPokemon) {
-      RandomizeHandler.prependPokemon(newPokemon)
-    }).fail(function() {
-      console.log("failed!")
-    })
-  },
+//   randomPokemon: function(e){
+//     e.preventDefault();
+//     var $newPokemonButton = $(this);
+//     $.ajax({
+//       url: $newPokemonButton.prop('action'),
+//       type: $newPokemonButton.prop('method')
+//     }).done(function(newPokemon) {
+//       RandomizeHandler.prependPokemon(newPokemon)
+//     }).fail(function() {
+//     })
+//   },
 
-  prependPokemon: function(newPokemon) {
-    $('#all-entries').prepend(newPokemon.pokemon_entry)
+//   prependPokemon: function(newPokemon) {
+//     $('#all-entries').prepend(newPokemon.pokemon_entry)
+//   }
+// };
+
+
+
+// $(document).ready(function() {
+//   RandomizeHandler.init()
+// })
+
+// The Rails Way
+var randomizeHandler = {
+
+  prependPokemon: function(e, newPokemon) {
+    $('#all-entries').prepend(newPokemon.pokemon_entry);
   }
 };
 
-
-
 $(document).ready(function() {
-  RandomizeHandler.init()
-})
+  $('form.random_pokemon').on('ajax:success', randomizeHandler.prependPokemon);
+});
