57ae8196881b573710d5a974f777bd97dbfb310c
Alek Sharma <aleksharma12@gmail.com>
2013-12-29 16:12:42 -0500

    Delete the old jQuery way comment

--- app/assets/javascripts/pokedex_entries.js
+++ app/assets/javascripts/pokedex_entries.js
@@ -1,34 +1,3 @@
-// The jQuery Way
-// var RandomizeHandler = {
-
-//   init: function(){
-//     $('form.random_pokemon').click(RandomizeHandler.randomPokemon)
-//   },
-
-//   randomPokemon: function(e){
-//     e.preventDefault();
-//     var $newPokemonButton = $(this);
-//     $.ajax({
-//       url: $newPokemonButton.prop('action'),
-//       type: $newPokemonButton.prop('method')
-//     }).done(function(newPokemon) {
-//       RandomizeHandler.prependPokemon(newPokemon)
-//     }).fail(function() {
-//     })
-//   },
-
-//   prependPokemon: function(newPokemon) {
-//     $('#all-entries').prepend(newPokemon.pokemon_entry)
-//   }
-// };
-
-
-
-// $(document).ready(function() {
-//   RandomizeHandler.init()
-// })
-
-// The Rails Way
 var randomizeHandler = {
 
   prependPokemon: function(e, newPokemon) {
