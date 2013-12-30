83a992bc1f5a1eefb5eda8406bf3b5f9912dae60
Alek Sharma <aleksharma12@gmail.com>
2013-12-29 20:49:23 -0500

    Add refactored class method with test for pokemon_entry

--- app/models/pokemon_entry.rb
+++ app/models/pokemon_entry.rb
@@ -1,4 +1,11 @@
 class PokemonEntry < ActiveRecord::Base
   validates_uniqueness_of :species_name
   validates_presence_of :species_name, :height, :weight
+
+  def self.random_pokemon_id
+    until false
+      target_pokemon = PokemonEntry.find(rand(151)+1)
+      return target_pokemon.id if (rand(255)+1) <= target_pokemon.rarity
+    end
+  end
 end
