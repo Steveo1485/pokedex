de8a57c123d883e24f841c0a97a6a14d2e20ccce
steveo1485 <stevennugent@gmail.com>
2014-01-09 16:57:20 -0800

    Create event_listeners.js for all event listeners, move modal event listeners to own function, call modal event listener function after ajax for random pokemon, create test for modal working on randomly caught pokemon

--- app/assets/javascripts/pokedex_entries.js
+++ app/assets/javascripts/pokedex_entries.js
@@ -2,6 +2,7 @@ var randomizeHandler = {
 
   prependPokemon: function(e, newPokemon) {
     $('#all-entries').prepend(newPokemon.pokemon_entry);
+    modalEventListeners();
   }
 };
 
@@ -9,8 +10,3 @@ function sortPokemon(e){
 	e.preventDefault();
 	$("#all-entries>div").tsort({attr:"id"});
 }
-
-$(document).ready(function() {
-  $('form.random_pokemon').on('ajax:success', randomizeHandler.prependPokemon);
-  $('form.sort-pokemon').submit(sortPokemon);
-});
