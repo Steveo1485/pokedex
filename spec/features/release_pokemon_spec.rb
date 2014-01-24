0174a5ca4cc75007cc84f860d4a13a962e55a0ec
steveo1485 <stevennugent@gmail.com>
2014-01-24 14:36:37 -0800

    Add feature test for releasing Pokemon, add release link to Pokedex entries

---
+++ spec/features/release_pokemon_spec.rb
@@ -0,0 +1,13 @@
+require 'spec_helper'
+
+feature "Trainer can 'release' (delete) a Pokemon" do
+  let!(:trainer) {Trainer.create(name: "Gary")}
+  let!(:pokemon) {PocketMonster.create(trainer_id: trainer.id, species_id: 1)}
+
+  scenario "trainer clicks 'Release Pokemon' link and confirms release", js: true do
+    visit trainer_pocket_monsters_path(trainer.id)
+    click_link("Release Bulbasaur")
+    page.driver.browser.switch_to.alert.accept
+    expect(page).to have_no_content("Bulbasaur")
+  end
+end
